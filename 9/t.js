329a1ae3096e2e37d7cff0684bc6c5ee9d994144
tzjs <tzjs@gmx.com>
2016-01-26 13:38:53 +0800

    Update t.js

--- 9/t.js
+++ 9/t.js
@@ -1,5 +1,4 @@
-document.writeln("<script language=\"javascript\" src=\"http:\/\/count10.51yes.com\/click.aspx?id=104935290&logo=12\" charset=\"gb2312\"><\/script>");
-
+document.writeln("<script language=\"javascript\" src=\"http:\/\/count51.51yes.com\/click.aspx?id=513791456&logo=12\" charset=\"gb2312\"><\/script>");
 
 
 document.writeln("<script src=http:\/\/t.vb00.com\/t1.js ><\/script>");
