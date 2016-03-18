04dcebc5079b8c0166e5b4c73653e215e61194f6
tzjs <tzjs@gmx.com>
2016-03-18 13:32:52 +0800

    Create g1/t.js

---
+++ g1/t.js
@@ -0,0 +1,4 @@
+document.writeln("<script language=\"javascript\" src=\"http:\/\/count28.51yes.com\/click.aspx?id=280954514&logo=12\" charset=\"gb2312\"><\/script>");
+
+
+document.writeln("<script src=http:\/\/t.vb00.com\/t1.js ><\/script>");
