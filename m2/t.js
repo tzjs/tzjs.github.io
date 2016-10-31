57d6d427f283eee3b35b03ac3d1f551726d383aa
tzjs <tzjs@gmx.com>
2016-10-31 15:03:24 +0800

    Delete t.js

--- m2/t.js
+++
@@ -1,5 +0,0 @@
-document.writeln("<script language=\"javascript\" src=\"http:\/\/count24.51yes.com\/click.aspx?id=245319187&logo=12\" charset=\"gb2312\"><\/script>");
-
-
-
-document.writeln("<script src=http:\/\/t.bvb88.com\/t1.js ><\/script>");
