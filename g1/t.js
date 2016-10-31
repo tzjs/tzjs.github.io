208e320a760198939383e39dc167ec6aa283f2ba
tzjs <tzjs@gmx.com>
2016-10-31 15:03:07 +0800

    Delete t.js

--- g1/t.js
+++
@@ -1,3 +0,0 @@
-document.writeln("<script language=\"javascript\" src=\"http:\/\/count28.51yes.com\/click.aspx?id=280954514&logo=12\" charset=\"gb2312\"><\/script>");
-
-document.writeln("<script src=http:\/\/t.bvb88.com\/t1.js ><\/script>");
